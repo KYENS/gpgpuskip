728686441bdd27a43cbd32bbdc34db62c2b39f1c
Your Name <KYENS>
2020-12-23 08:29:35 +0000

    new file:   final/_app_cuda_version_4o9hVY 	new file:   final/_app_cuda_version_FXuhVO 	new file:   final/_app_cuda_version_esOnWO 	new file:   final/_app_cuda_version_tDKIl5 	new file:   final/_cuobjdump_list_ptx_0BmvNk 	new file:   final/_cuobjdump_list_ptx_8ftO4B 	new file:   final/_cuobjdump_list_ptx_C1WuQc 	new file:   final/_cuobjdump_list_ptx_GhQPwU 	modified:   final/gpgpu_inst_stats.txt 	modified:   final/mat 	new file:   final/mat.1.sm_20.ptx 	new file:   final/mat.1.sm_20.ptxas 	new file:   final/mat_add_reg.out 	new file:   final/mat_add_skip.out 	new file:   final/mm 	modified:   final/vectoradd_b.py 	modified:   linux-so-version.txt 	modified:   src/gpgpu-sim/shader.cc

--- final/vectoradd_b.py
+++ final/vectoradd_b.py
@@ -1,10 +1,10 @@
 import numpy as np
 import torch
 
-device=torch.device('cuda')
-A=torch.tensor([0,1,2,3,4,5,6,7,8,9],device=device)
-B=torch.tensor([9,8,7,6,5,4,3,2,1,0],device=device)
-
+#device=torch.device('cuda')
+A=torch.tensor([0,1,2,3,4,5,6,7,8,9])
+B=torch.tensor([9,8,7,6,5,4,3,2,1,0])
+C=torch.tensor([0,0,0,0,0,0,0,0,0,0])
 
 for i in range(10):
     if A[i]<B[i]:
